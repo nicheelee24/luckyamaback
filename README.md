b247dc8fffdf68069aaa5249b547d45e85875780
koiescafe2024 <nicheelee24@gmail.com>
2024-10-25 17:30:31 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# luckyamaback
+# Env
+FRONTEND_URL = 'http://localhost:3000'
+MONGODB_USER = ''
+MONGODB_PASSWORD = ''
+AWC_HOST = 'h'
+AWC_CERT = ''
+AWC_AGENT_ID = ''
+JWT_SECRET = 'jwt_secret'
+
+# dotbet-backend
---
+++ routes/api/promotion.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const axios = require("axios");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+// const { getIo } = require('../../socketManager');
+
+const Promotion = require("../../models/Promotion");
+const User = require("../../models/User");
+
+router.get("/getpromotions", async (req, res) => {
+    try {
+        console.log("req.query", req.query)
+        let promotionsList = [];
+        if (req.query?.expired == 'false') {
+            promotionsList = await Promotion.find({
+                $expr: {
+                    $gt: [
+                        {
+                            $dateFromString: {
+                                dateString: {
+                                    $concat: [
+                                        { $substr: ["$expDate", 6, 4] }, "-", // Extract and format year
+                                        { $substr: ["$expDate", 3, 2] }, "-", // Extract and format month
+                                        { $substr: ["$expDate", 0, 2] }      // Extract and format day
+                                    ]
+                                },
+                                format: "%Y-%m-%d"
+                            }
+                        },
+                        {
+                            $dateFromString: {
+                                dateString: {
+                                    $dateToString: { format: "%Y-%m-%d", date: new Date() }
+                                },
+                                format: "%Y-%m-%d"
+                            }
+                        }
+                    ]
+                }
+            });
+        } else {
+            promotionsList = await Promotion.find({
+                agentname: "luckyama",
+            }).limit(50);
+        }
+        console.log("data....." + promotionsList)
+        res.json({ status: "0000", promotionsList });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+module.exports = router;
