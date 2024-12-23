a6652f0b0f4776d0e2a2f4450cf43d51275959b4
koiescafe2024 <nicheelee24@gmail.com>
2024-12-23 18:41:18 +0530

    Update pay.js

--- routes/api/pay.js
+++ routes/api/pay.js
@@ -1468,7 +1468,7 @@ router.post("/balance", auth, async (req, res) => {
 	}
 	console.log("trans result" + resultTrans.length);
 	let totalBetAmount = 0;
-	let totalTurnover = resultTrans[0].totalTransAmount;
+	let totalTurnover = 0;
 	console.log("bet total result.." + result);
 	// console.log("transaction amount..deposit.." + resultTrans[0].totalTransAmount);
 
